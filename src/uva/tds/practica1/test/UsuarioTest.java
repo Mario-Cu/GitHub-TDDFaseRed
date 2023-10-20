524c003439f9316a37d048a8ba305b1f966536f6
marcobr <mario.cobreros@alumnos.uva.es>
2023-10-20 17:08:25 +0200

    Creado el test de usuario y los paquetes necesarios para las clases

---
+++ src/uva/tds/practica1/test/UsuarioTest.java
@@ -0,0 +1,14 @@
+package uva.tds.practica1.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

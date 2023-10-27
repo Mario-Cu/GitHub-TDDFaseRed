fa35cc410d82457b2d8f890bdddd428b55372a95
marcobr <mario.cobreros@alumnos.uva.es>
2023-10-27 17:32:25 +0200

    Añadido parte del javadoc

--- src/uva/tds/practica1/usuario/Usuario.java
+++ src/uva/tds/practica1/usuario/Usuario.java
@@ -1,9 +1,18 @@
 package uva.tds.practica1.usuario;
 
+/**
+ * Clase que representa un usuario.
+ */
 public class Usuario {
-	String nif;
-	String nombre;
 	
+	/**
+	 * Creacion de un usuario
+	 * @param nif Identificador de una persona. Debe tener 9 caracteres
+	 * @param nombre Palabra con la que se denomina a una persona. Debe tener entre 1 y 15 caracteres
+	 * @throws IllegalArgumentException si la longitud de nif es mayor o menor que 9 
+	 * @throws IllegalArgumentException si nif utiliza las letras I,Ñ,O,U.
+	 * @throws IllegalArgumentException si el resto de la division del numero nif entre 23 no coincide con su letra asociada 
+	 */
 	public Usuario(String nif, String nombre) {
 		
 	}

a6cf5e194816c2241857b36135b8e4c1bf07b0a2
marcobr <mario.cobreros@alumnos.uva.es>
2023-11-03 16:23:21 +0100

    Realizados los tests relacionados con la clase Billete
    Añadidas temporalmente las clases Recorrido y Usuario para comprobar
    funcionamiento

--- src/uva/tds/practica1/billete/test/BIlleteTest.java
+++ src/uva/tds/practica1/billete/test/BIlleteTest.java
@@ -1,45 +1,81 @@
 package uva.tds.practica1.billete.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BIlleteTest {
+import uva.tds.practica1.recorrido.Recorrido;
+import uva.tds.practica1.usuario.Usuario;
 
-	@Test
-	void testConstructorBilleteLocalizadorLimiteInferior() {
-		Billete billete = new Billete("L",recorrido,usuario);
-		assertEquals("L",billete.getLocalizador());
-		//assertSame("Recorrido",recorrido.getX());
-		//assertSame("Usuario",usuario.getX());
+class BIlleteTest {
+	private Usuario usrNormal;
+	private Recorrido recorridoNormal;
+	private LocalDate fecha;
+	private LocalTime hora;
+
+	
+	@BeforeEach
+	void setUp() {
+		this.usrNormal = new Usuario("33036946E","UsuarioNormal");
+		fecha = LocalDate.of(2002, 7, 18);
+		hora = LocalTime.of(12, 30);
+		this.recorridoNormal = new Recorrido("1","origen","destino","autobus",0,fecha,hora,1,1);
+
 	}
 	
 	@Test
 	void testConstructorBilleteLocalizadorLimiteInferior() {
+		Billete billete = new Billete("L",recorridoNormal,usrNormal);
+		assertEquals("L",billete.getLocalizador());
+		assertEquals("1",recorridoNormal.getId());
+		assertEquals("UsuarioNormal",usrNormal.getNombre());
+	}
+	
+	@Test
+	void testConstructorBilleteLocalizadorLimiteSuperior() {
 		Billete billete = new Billete("Loc8cara",recorrido,usuario);
 		assertEquals("Loc8cara",billete.getLocalizador());
-		//assertSame("Recorrido",recorrido.getX());
-		//assertSame("Usuario",usuario.getX());
+		assertEquals("1",recorridoNormal.getId());
+		assertEquals("UsuarioNormal",usrNormal.getNombre());
 	}
 
 	@Test
 	void testConstructorBilleteNoValidoLocalizadorMenorLimiteInferior() {
 		assertThrows(IllegalArgumentException.class,()->{
-			new Billete("",recorrido,usuario);
+			new Billete("",recorridoNormal,usrNormal);
 		});
 	}
 	
 	@Test
 	void testConstructorBilleteNoValidoLocalizadorMayorQueLimiteSuperior() {
 		assertThrows(IllegalArgumentException.class,()->{
-			new Billete("Loc8carac",recorrido,usuario);
+			new Billete("Loc8carac",recorridoNormal,usrNormal);
 		});
 	}
 	
 	@Test
 	void testConstructorBilleteNoValidoLocalizadorNulo() {
 		assertThrows(IllegalArgumentException.class,()->{
-			new Billete(null,recorrido,usuario);
+			new Billete(null,recorridoNormal,usrNormal);
+		});
+	}
+	
+	@Test
+	void testConstructorBilleteNoValidoRecorridoNulo() {
+		assertThrows(IllegalArgumentException.class,()->{
+			new Billete("LocNorm",null,usrNormal);
+		});
+	}
+	
+	@Test
+	void testConstructorBilleteNoValidoUsuarioNulo() {
+		assertThrows(IllegalArgumentException.class,()->{
+			new Billete("LocNorm",recorridoNormal,null);
 		});
 	}
 

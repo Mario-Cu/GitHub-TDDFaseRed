62b5969d370b0e3aecc28ef5e8121c1082bc36d4
Mario <mariodanovivanov@gmail.com>
2023-11-03 17:10:05 +0100

    Finalizados tests para la F4

--- src/uva/tds/practica1/sistema/test/SistemaTest.java
+++ src/uva/tds/practica1/sistema/test/SistemaTest.java
@@ -6,37 +6,74 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uva.tds.practica1.recorrido.*;
-
+import uva.tds.practica1.billete.*;
+import uva.tds.practica1.usuario.*;
 class SistemaTest {
 	private Recorrido recorrido1;
 	private Recorrido recorrido2;
-	private Recorrido recorrido3;
+	private Usuario usuario;
 	private LocalDate fecha;
 	private LocalTime hora;
-	
+
+
+		
 	@BeforeEach
 	void setUp() throws Exception {
 		fecha = LocalDate.of(2002, 7, 18);
 		hora = LocalTime.of(12, 30);
 		recorrido1 = new Recorrido("1","origen","destino","autobus",0,fecha,hora,1,1);
 		recorrido2 = new Recorrido("2","origen","destino","autobus",0,fecha,hora,1,1);
-		recorrido3 = new Recorrido("3","origen","destino","autobus",0,fecha,hora,1,1);
+		usuario = new Usuario("33036946E","UsuarioNormal");
 	}
-
 	@Test
 	void testAñadirRecorridoAlSistema(){
 		Sistema sistema = new Sistema();
 		sistema.añadirRecorrido(recorrido1);
 		assertTrue(sistema.getRecorridos.contains(recorrido1));
 	}
+	@Test	
 	void testAñadirRecorridoAlSistemaNoValidoDosRecorrridosConMismoIdentificador() {
-		
-		assertThrows(IllegalStateException.class, () ->{
 			Recorrido recorrido1Copia = new Recorrido("1","origen","destino","autobus",0,fecha,hora,1,1);
 			Sistema sistema = new Sistema();
 			sistema.añadirRecorrido(recorrido1);
+		assertThrows(IllegalStateException.class, () ->{
 			sistema.añadirRecorrido(recorrido1Copia);
 		});
 	}
+	@Test
+	void testEliminarRecorridoDelSistema() {
+		Sistema sistema = new Sistema();
+		sistema.añadirRecorrido(recorrido1);
+		sistema.añadirRecorrido(recorrido2);
+		sistema.eliminarRecorrido(recorrido2.getId());
+		assertFalse(sistema.getRecorridos.contains(recorrido2));
+	}
+	@Test
+	void testEliminarRecorridoDelSistemaNoValidoRecorridoConBilletesAsociados() {
+		Sistema sistema = new Sistema();
+		Usuario usuario = new Usuario("33036946E","UsuarioNormal");
+		Billete billete = new Billete("LocNorm",recorrido1,usuario);
+		sistema.añadirRecorrido(recorrido1);
+		assertThrows(IllegalStateException.class, () ->{
+			sistema.eliminarRecorrido(recorrido1);
+		});
+	}
+	@Test
+	void testActualizarRecorridoFecha() {
+		Sistema sistema = new Sistema();
+		LocalDate fechaNueva = LocalDate.of(2002, 7, 19);
+		sistema.añadirRecorrido(recorrido1);
+		sistema.actualizarFechaRecorrido(recorrido1.getId(),fechaNueva);
+		assertEquals(fechaNueva,recorrido1.getFecha());
+	}
+	@Test
+	void testActualizarRecorridoHora() {
+		Sistema sistema = new Sistema();
+		LocalTime horaNueva = hora = LocalTime.of(13, 00);
+		sistema.añadirRecorrido(recorrido1);
+		sistema.actualizarHoraRecorrido(recorrido1.getId(),horaNueva);
+		assertEquals(horaNueva,recorrido1.getHora());
+	}
+	
 
 }
